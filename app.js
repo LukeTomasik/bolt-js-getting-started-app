3542427371c41390976bbe874768c1e93214689d
NoxYeah <Luke.tomasik@gmail.com>
2022-11-29 16:43:12 -0600

    inital commit

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const { App } = require('@slack/bolt');
+const { App, AwsLambdaReceiver } = require('@slack/bolt');
+// Store environmental variables configuration
+require("dotenv").config();
 
 /* 
 This sample slack application uses SocketMode
@@ -7,10 +9,15 @@ see: https://slack.dev/bolt-js/tutorial/getting-started
 */
 
 // Initializes your app with your bot token and app token
+const awsLambdaReceiver = new AwsLambdaReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN
+  receiver: awsLambdaReceiver,
 });
 
 // Listens to incoming messages that contain "hello"
@@ -38,15 +45,21 @@ app.message('hello', async ({ message, say }) => {
   });
 });
 
+// Listens for an action from a button click
 app.action('button_click', async ({ body, ack, say }) => {
-  // Acknowledge the action
   await ack();
+  
   await say(`<@${body.user.id}> clicked the button`);
 });
 
-(async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
+// Listens to incoming messages that contain "goodbye"
+app.message('goodbye', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`See ya later, <@${message.user}> :wave:`);
+});
 
-  console.log('⚡️ Bolt app is running!');
-})();
+
+module.exports.handler = async (event, context, callback) => {
+  const handler = await awsLambdaReceiver.start();
+  return handler(event, context, callback);
+}
